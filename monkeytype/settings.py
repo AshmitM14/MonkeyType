80798b5ae7a115d62a9a1b3f221c2a244c71e10a
Ashmit <ashmitmahindroo155@gmail.com>
2023-06-21 11:51:34 +0530

    commit

--- monkeytype/settings.py
+++ monkeytype/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-np(y8&_0%9gani@80hshuz-jp!vxihkt0!3dg&ktz@*c)umph-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
